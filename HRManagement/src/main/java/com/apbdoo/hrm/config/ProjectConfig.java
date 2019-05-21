089167813e3641a53b3e88b4a72eacbc1fc722b1
Alexandru Enache <alexandru.enache@cegeka.com>
2019-05-21 15:02:59 +0300

    .

--- HRManagement/src/main/java/com/apbdoo/hrm/config/ProjectConfig.java
+++ HRManagement/src/main/java/com/apbdoo/hrm/config/ProjectConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.dialect.SpringStandardDialect;
@@ -48,5 +49,10 @@ public class ProjectConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login.html");
     }
 }
